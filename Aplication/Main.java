5975ca6cce3a1f4fbfcc03e1821eff86a8d1ebd6
wolfblack27 <tigowolfblack27@gmail.com>
2023-10-16 18:22:40 -0300

    Criadas as classes ContaPoupanca e metodos

--- Aplication/Main.java
+++ Aplication/Main.java
@@ -1,14 +1,40 @@
 package Aplication;
 
+import Entites.Conta;
 import Entites.ContaNegocio;
+import Entites.ContaPoupanca;
 
 public class Main {
 
 	public static void main(String[] args) {
 		
-		ContaNegocio cn = new ContaNegocio(1201, "Thiago Ventura", 100.00, 500.00);
-		cn.deposit(300);
-		System.out.println(cn.getBalance());
+		
+		Conta acc = new Conta(1001, "Alex", 0.0);
+		ContaNegocio bacc = new ContaNegocio(1002, "Maria", 0.0, 500.0);
+		
+		// UPCASTING
+		
+		Conta acc1 = bacc;
+		Conta acc2 = new ContaNegocio(1003, "Bob", 0.0, 200.0);
+		Conta acc3 = new ContaPoupanca(1004, "Anna", 0.0, 0.01);
+		
+		// DOWNCASTING
+		
+		ContaNegocio acc4 = (ContaNegocio)acc2;
+		acc4.loan(100.0);
+		
+		// BusinessAccount acc5 = (BusinessAccount)acc3;
+		if (acc3 instanceof ContaNegocio) {
+			ContaNegocio acc5 = (ContaNegocio)acc3;
+			acc5.loan(200.0);
+			System.out.println("Loan!");
+		}
+		
+		if (acc3 instanceof ContaPoupanca) {
+			ContaPoupanca acc5 = (ContaPoupanca)acc3;
+			acc5.updateBalance();
+			System.out.println("Update!");
+		}
 		
 	}
 
---
+++ Entites/ContaPoupanca.java
@@ -0,0 +1,35 @@
+package Entites;
+
+public class ContaPoupanca  extends Conta{
+
+	
+private Double interestRate;
+	
+	public ContaPoupanca() {
+		super();
+	}
+
+	public ContaPoupanca(Integer number, String holder, Double balance, Double interestRate) {
+		super(number, holder, balance);
+		this.interestRate = interestRate;
+	}
+
+	public Double getInterestRate() {
+		return interestRate;
+	}
+
+	public void setInterestRate(Double interestRate) {
+		this.interestRate = interestRate;
+	}
+	
+	public void updateBalance() {
+		balance += balance * interestRate;
+	}
+	
+	
+	
+	
+	
+	
+	
+}
